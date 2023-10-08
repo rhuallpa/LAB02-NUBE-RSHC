bdd967132ca3361658a6a08b3ef0403124eeb2a5
rhuallpa <rebeca.huallpa@tecsup.edu.pe>
2023-10-08 17:43:59 -0500

    test

--- mi_proyecto/settings.py
+++ mi_proyecto/settings.py
@@ -90,10 +90,10 @@ WSGI_APPLICATION = 'mi_proyecto.wsgi.application'
 
 
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
