350cb37c5d03cfe837d5be2522bdb84abdbeb20b
rhuallpa <rebeca.huallpa@tecsup.edu.pe>
2023-10-08 12:23:49 -0500

    test

--- mi_proyecto/settings.py
+++ mi_proyecto/settings.py
@@ -27,7 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Aplicaciones.Academicos',
 ]
 
 MIDDLEWARE = [
@@ -124,6 +124,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
+APLICACIONES_URL = 'aplicaciones/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- mi_proyecto/urls.py
+++ mi_proyecto/urls.py
@@ -19,7 +19,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
+    path('',include('Aplicaciones.Academicos.urls')),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
